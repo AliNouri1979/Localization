fd99ea83b145ffd52d7f9301ee83b81057afc48e
Manager <Manager@MANAGER-PC>
2019-10-17 18:28:12 +0330

    Add project files.

---
+++ Localization/Program.cs
@@ -0,0 +1,32 @@
+﻿/****************************** Ali Nouri ******************************\
+*	Module Name:	Program.cs
+*	Project:		Localization
+*	Copyright (C) 2019 Ali Nouri, All rights reserved.
+*	This software may be modified and distributed under the terms of the MIT license.  See LICENSE file for details.
+*
+*	Written by Ali Nouri <alinouri1979@gmail.com>,  10, 17, 2019, 5:50 PM
+*
+***********************************************************************************/
+
+using DevExpress.UserSkins;
+using System;
+using System.Windows.Forms;
+
+namespace Localization
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            BonusSkins.Register();
+            Application.Run(new SelectedLanguageForm());
+        }
+    }
+}
